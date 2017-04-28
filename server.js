60f26744fb4b78643fc1e837020b7f0ad7651b6d
puneet <puneetvashsiht@gmail.com>
2017-04-28 14:10:12 +0530

    test

--- server.js
+++ server.js
@@ -7,6 +7,19 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+var port     = process.env.NODE_PORT || 4000;
+var host     = process.env.NODE_IP;
+
+if(host) {
+	http.listen(port, host, () => {
+        logger.info("The magic happens on  " + host + ":" + port);
+	});
+} else {
+	http.listen(port, () => {
+        logger.info("The magic happens on:" + port);
+	});
+}
+
 app.listen(8000, function () {
   console.log('Example app listening on port 3000!')
 })
